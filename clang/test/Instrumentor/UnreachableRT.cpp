2d46ea3bbdf521e6b5064e291559465ad3a01462
Johannes Doerfert <jdoerfert.llvm@gmail.com>
2026-05-01 21:21:25 -0700

    [Instrumentor] Add unreachable support; unreachable stack trace printing
    Allow to instrument unreachable and provide a use case for stack trace
    printing.

---
+++ clang/test/Instrumentor/UnreachableRT.cpp
@@ -0,0 +1,21 @@
+//===---- Instrumentor/UnreachableRT.c - An example Instrumentor use -----===//
+//
+// Part of the LLVM Project, under the Apache License v2.0 with LLVM Exceptions.
+// See https://llvm.org/LICENSE.txt for license information.
+// SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
+//
+//===----------------------------------------------------------------------===//
+//
+//
+//===----------------------------------------------------------------------===//
+
+#include "llvm/Support/Signals.h"
+#include "llvm/Support/raw_ostream.h"
+
+extern "C" {
+void __unreachable_pre_unreachable(int64_t ID) { 
+  llvm::outs() << "Hit unreachable #" << ID << "\n";
+  llvm::sys::PrintStackTrace(llvm::outs());
+  std::exit(0);
+}
+}
